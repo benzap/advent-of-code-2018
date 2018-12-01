fc36d5fa4610feb745690ad3e5cde0be13b775b5
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-12-01 15:29:45 -0500

    Finished first part of problem 1

--- src/bin/p1.rs
+++ src/bin/p1.rs
@@ -1,13 +1,30 @@
 // Problem 1
+extern crate regex;
 
+use std::collections::HashSet;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
 
 fn main() {
-    let fileHandle = File::open("resources/p1.txt").unwrap();
-    let bufReader = BufReader::new(&fileHandle);
-    for line in bufReader.lines() {
-        println!("{}\t", line.unwrap());
+    let file_handle = File::open("resources/p1.txt").unwrap();
+    let buffer_reader = BufReader::new(&file_handle);
+
+    let mut twice = None;
+    let mut frequency_hit = HashSet::new();
+    let mut total = 0;
+    for line in buffer_reader.lines() {
+        let l = line.unwrap();
+        let value = l.parse::<i32>().unwrap();
+        total += value;
+        if frequency_hit.contains(&total) && twice == None {
+            twice = Some(total.clone());
+        }
+        else {
+            frequency_hit.insert(total.clone());
+        }
     }
+
+    println!("The total frequency: {}", total);
+    println!("First Frequency hit Twice: {}", twice.unwrap());
 }
