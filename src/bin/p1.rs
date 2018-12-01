a23239137087adcf04eaf19e834aa6e872c08d95
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-12-01 15:45:31 -0500

    First attempt at part 2, failed submit

--- src/bin/p1.rs
+++ src/bin/p1.rs
@@ -6,25 +6,40 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
 
+fn get_first_frequency_double() -> i32 {
+    let mut total = 0;
+    let mut frequency_hit = HashSet::new();
+    while(true) {
+        let file_handle = File::open("resources/p1.txt").unwrap();
+        let buffer_reader = BufReader::new(&file_handle);
+        for line in buffer_reader.lines() {
+            let l = line.unwrap();
+            let value = l.parse::<i32>().unwrap();
+            total += value;
+
+            if (frequency_hit.contains(&total)) {
+                return total;
+            }
+            else {
+                frequency_hit.insert(value.clone());
+            }
+        }
+    }
+    total
+}
+
 fn main() {
     let file_handle = File::open("resources/p1.txt").unwrap();
     let buffer_reader = BufReader::new(&file_handle);
 
-    let mut twice = None;
-    let mut frequency_hit = HashSet::new();
+
     let mut total = 0;
     for line in buffer_reader.lines() {
         let l = line.unwrap();
         let value = l.parse::<i32>().unwrap();
         total += value;
-        if frequency_hit.contains(&total) && twice == None {
-            twice = Some(total.clone());
-        }
-        else {
-            frequency_hit.insert(total.clone());
-        }
     }
 
     println!("The total frequency: {}", total);
-    println!("First Frequency hit Twice: {}", twice.unwrap());
+    println!("First Frequency hit Twice: {}", get_first_frequency_double());
 }
