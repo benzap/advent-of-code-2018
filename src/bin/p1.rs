1a35786c3547fbd8a32fe2307dacea305bf62b7b
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-12-01 21:48:03 -0500

    Cleaned up P1 Code

--- src/bin/p1.rs
+++ src/bin/p1.rs
@@ -5,12 +5,15 @@ use std::collections::HashSet;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
+use std::io::SeekFrom;
+use std::io::Seek;
 
 fn get_first_frequency_double() -> i32 {
     let mut total : i32 = 0;
     let mut frequency_hit = HashSet::new();
+    let mut file_handle = File::open("resources/p1.txt").unwrap();
     loop {
-        let file_handle = File::open("resources/p1.txt").unwrap();
+        file_handle.seek(SeekFrom::Start(0)).expect("Failed to seek file");
         let buffer_reader = BufReader::new(&file_handle);
         for line in buffer_reader.lines() {
             let l = line.unwrap();
@@ -31,7 +34,6 @@ fn main() {
     let file_handle = File::open("resources/p1.txt").unwrap();
     let buffer_reader = BufReader::new(&file_handle);
     
-
     let mut total = 0;
     for line in buffer_reader.lines() {
         let l = line.unwrap();
