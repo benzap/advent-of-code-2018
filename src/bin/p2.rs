709bfb3f4c78a3628842e01848814ef5dbf290b7
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-12-02 09:56:31 -0500

    Finished P1 Part 2

--- src/bin/p2.rs
+++ src/bin/p2.rs
@@ -18,7 +18,7 @@ struct IdResult {
 }
 
 
-fn generate_id_result(s: String) -> IdResult {
+fn generate_id_result(s: &String) -> IdResult {
     let mut m = HashMap::new();
     for c in s.chars() {
         if let Some(x) = m.get_mut(&c) {
@@ -43,13 +43,58 @@ fn generate_id_result(s: String) -> IdResult {
 }
 
 
-fn main() {
+fn get_id_listing() -> Vec<String> {
     let file_handle = File::open("resources/p2.txt").unwrap();
     let buffer_reader = BufReader::new(&file_handle);
-    let mut result_listing = Vec::new();
+    let mut id_listing = Vec::new();
     for line in BufReader::new(&file_handle).lines() {
         let l = line.unwrap();
-        result_listing.push(generate_id_result(l));
+        id_listing.push(l);
+    }
+    id_listing
+}
+
+
+fn generate_diff_complement(id_listing: &Vec<String>,
+                            id_test: &String,
+                            id_index: usize) -> String {
+    let mut best_diff = String::new();
+    for (index, id) in id_listing.iter().enumerate() {
+        if id_index == index { continue; }
+        let mut cdiff = String::new();
+        for i in 0..id.len() {
+            let a = id_test.chars().collect::<Vec<char>>()[i];
+            let b = id.chars().collect::<Vec<char>>()[i];
+            if a == b {
+                cdiff.push(a);
+            }
+        }
+        
+        if best_diff.len() < cdiff.len() {
+            best_diff = cdiff.clone();
+        }
+    }
+    best_diff
+}
+
+
+fn get_best_box_id(id_listing: &Vec<String>) -> String {
+    let mut best_cdiff = String::new();
+    for (index, id) in id_listing.iter().enumerate() {
+        let check_cdiff = generate_diff_complement(id_listing, id, index);
+        if best_cdiff.len() < check_cdiff.len() {
+            best_cdiff = check_cdiff.clone();
+        }
+    }
+    best_cdiff
+}
+
+
+fn main() {
+    let id_listing = get_id_listing();
+    let mut result_listing = Vec::new();
+    for id in &id_listing {
+        result_listing.push(generate_id_result(id));
     }
 
     let mut num2 = 0u32;
@@ -63,5 +108,6 @@ fn main() {
         }
     }
 
-    println!("Resulting Checksum {}", num2 * num3);
+    println!("Resulting Checksum: {}", num2 * num3);
+    println!("Best ID Diff Complement: {}", get_best_box_id(&id_listing));
 }
